b8dd497c2209d0903bac07c53c14154ff4c11fdb
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2024-03-24 20:23:00 +0100

    block moving added

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import pygame
 from pygame.locals import *
 
+def draw_block():
+    board.fill((110, 110, 5))
+    board.blit(block, (block_x, block_y))
+    pygame.display.flip()
 
 
 if __name__ == '__main__':
     pygame.init()
-    surface = pygame.display.set_mode((500, 500))
-    surface.fill((110, 110, 5))
+    board = pygame.display.set_mode((500, 500))
+    board.fill((110, 110, 5))
+
+    block = pygame.image.load("resources/block.jpg").convert()
+    block_x = 100
+    block_y = 100
+    board.blit(block, (block_x, block_y))
+
+
     pygame.display.flip()
 
     running = True
@@ -16,5 +27,21 @@ if __name__ == '__main__':
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     running = False
+
+                if event.key == K_UP:
+                    block_y -= 10
+                    draw_block()
+                if event.key == K_DOWN:
+                    block_y += 10
+                    draw_block()
+                if event.key == K_LEFT:
+                    block_x -= 10
+                    draw_block()
+                if event.key == K_RIGHT:
+                    block_x += 10
+                    draw_block()
+               
+                    
+                    
             if event.type == QUIT:
                 running = False   
