2b976db1c21a6160412d1c726774106aa015a7b7
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2024-04-03 20:38:22 +0200

    Snake length added and movement changed

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import pygame
 from pygame.locals import *
 import time
 
+SIZE = 40
 
 
 class Snake:
-    def __init__(self, parent_screen):
+    def __init__(self, parent_screen, length):
+        self.length = length
         self.parent_screen = parent_screen
         self.block = pygame.image.load("resources/block.jpg").convert()
-        self.x = 100
-        self.y = 100
+        self.x = [SIZE] * self.length
+        self.y = [SIZE] * self.length
         self.direction = 'down'
         
     def draw(self):
         self.parent_screen.fill((110, 110, 5))
-        self.parent_screen.blit(self.block, (self.x, self.y)) 
+        for i in range(self.length):
+            self.parent_screen.blit(self.block, (self.x[i], self.y[i])) 
         pygame.display.flip()
         
     def move_left(self):
@@ -30,22 +33,27 @@ class Snake:
         self.direction = 'down' 
      
     def walk(self):
+    # kolejny blok przyjmuje współrzędne poprzedniego
+        for i in range(self.length-1, 0, -1):
+            self.x[i] = self.x[i - 1]
+            self.y[i] = self.y[i - 1]
+        
         if self.direction == 'up':
-            self.y -= 10
+            self.y[0] -= SIZE
         if self.direction == 'down':
-            self.y += 10
+            self.y[0] += SIZE
         if self.direction == 'left':
-            self.x -= 10
+            self.x[0] -= SIZE
         if self.direction == 'right':
-            self.x += 10
+            self.x[0] += SIZE
         self.draw()   
 
 class Game:
     def __init__(self):            
         pygame.init()  
-        self.board = pygame.display.set_mode((500, 500))
+        self.board = pygame.display.set_mode((1200, 800))
         self.board.fill((110, 110, 5))
-        self.snake = Snake(self.board)
+        self.snake = Snake(self.board, 6)
         self.snake.draw()
 
 
